55532a6d9d77c5083c7ff6aa735d0cccbef41893
Licho <l.ojedafernandez@gmail.com>
2021-05-14 17:41:42 -0300

    Se creo una clase abstracta y se hizo heredar el resto

--- src/ar/unr/model/Empresa.java
+++ src/ar/unr/model/Empresa.java
@@ -5,22 +5,18 @@ import java.util.stream.Stream;
 
 public class Empresa {
 
-    private static ArrayList<String> puesto = new ArrayList<>();
     private String nombre;
     private Empleado director;
 
 
-    public Empresa(String nombre, ArrayList<String> puestos, Empleado director){
+    public Empresa(String nombre, Empleado director){
         if (nombre == null ||nombre.isEmpty())
             throw new RuntimeException("Debe cargar un nombre");
-        if (puestos.size()==0)
-            throw new RuntimeException("Debe cargar la estructura de la empresa");
         if (director ==null){
             throw new RuntimeException("La empresa tiene que tener un director");
         }
         this.nombre = nombre;
         this.director = director;
-        puesto = puestos;
     }
 
     public Double generarSueldosDeEmpleados(){
